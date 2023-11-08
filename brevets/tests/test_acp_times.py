c76dbed63c4c1466e6029d86ec9fffddd6d37a5b
root <root@LAPTOP-2ITC3CV2>
2023-11-07 17:07:06 -0800

    completed_acp_times_test

--- brevets/tests/test_acp_times.py
+++ brevets/tests/test_acp_times.py
@@ -7,7 +7,7 @@ Write your tests HERE AND ONLY HERE.
 import nose    # Testing framework
 import logging
 import arrow
-import brevets.acp_times import open_time, close_time # important function open_time and close_time from acp_times
+import acp_times import open_time, close_time # important function open_time and close_time from acp_times
 
 logging.basicConfig(format='%(levelname)s:%(message)s',
                     level=logging.WARNING)
@@ -15,22 +15,32 @@ log = logging.getLogger(__name__)
 
 
 def test_open_time():
+    # test that the times are correctly given for open if there are 0 km
     start_time = arrow.get("2023-11-04T09:20:00")
     assert acp_times.open_time(0, 200, start_time) == start_time
     assert acp_times.open_time(0, 1000, start_time) == start_time
     
-    assert close_time(200,200, arrow.get("2023-11-04T12:00:00")) == arrow.get("2023-11-04T05:53:00")
+    # test that the correct times are produced
+    assert open_time(200,200, arrow.get("2023-11-04T12:00:00")) == arrow.get("2023-11-04T05:53:00")
     assert open_time(400,400, arrow.get("2023-11-04T12:00:00")) == arrow.get("2023-11-04T12:30:00")
 
 def test_close_time():
+    # test that the close start adjust the time correctly if the distance is 0 km
     start_time = arrow.get("2023-11-04T12:00:00")
     assert close_time(0, 200, start_time) != start_time
     assert close_time(0, 200, start_time) == start_time.shift(hours=1)
     assert close_time(0, 1000, start_time) == start_time.shift(hours=1)
 
+    # test that any distnace under 60 km is calulated correctly
+    assert close_time(10,200, arrow.get("2023-11-04T00:00:00")) == arrow.get("2023-11-04T01:30:00")
+    assert close_time(50,200, arrow.get("2023-11-04T00:00:00")) == arrow.get("2023-11-04T03:30:00")
+
+    # test that the the distances for brevts 200 and 400 have their time adjusted correctly
     assert close_time(200,200, start_time) == arrow.get("2023-11-04T01:30:00")
     assert close_time(200,200, start_time) != arrow.get("2023-11-04T01:20:00")
-    assert close_time(400,400, start_time) == arrow.get("2023-11-04T03:00:00")
+    assert open_time(400,400, arrow.get("2023-11-04T12:00:00")) == arrow.get("2023-11-04T03:00:00")
+
+    #test that this is the correct time for 1000 km
     assert close_time(1000,1000, start_time) ==  arrow.get("2023-11-04T03:00:00")
 
 
